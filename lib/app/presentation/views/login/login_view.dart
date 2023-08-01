8f9de834a20645293bb03642e454ffd300d2ae49
Juan Canon <jccaon44@misena.edu.co>
2023-08-01 16:28:30 -0500

    se creo la interfaz de login_view.dart y se configuro las dependencias xxxx ademas se actualizo el build.grade a la version 19

--- lib/app/presentation/views/login/login_view.dart
+++ lib/app/presentation/views/login/login_view.dart
@@ -1,39 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spadeunas/app/presentation/widgets/form_text_field.dart';
+import 'package:spadeunas/app/presentation/widgets/my_buttom_form.dart';
+
+import '../../widgets/my_from_text_field.dart';
 
 class LoginView extends StatelessWidget {
-
   static const String name = 'login_view';
 
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
-        child: SafeArea(
-          child: Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                //Logo
-                SvgPicture.asset('assets/images/logo.svg',
-                  colorFilter:  ColorFilter.mode(
-                      Theme.of(context).colorScheme.primary, BlendMode.srcIn
+          child: SafeArea(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //Logo
+                  SvgPicture.asset(
+                    'assets/images/MyMarca.svg',
+                    colorFilter: ColorFilter.mode(
+                        Theme.of(context).colorScheme.primary, BlendMode.srcIn),
                   ),
-                ),
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  const MyFormTextField(
+                    labelText: 'Correo Electrónico',
+                    hintText: 'Ingresa tu correo electrónico ',
+                    textInputType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    suffixIcon: false,
+                  ),
+                  const SizedBox(height: 20,),
+                  const MyFormTextField(
+                    labelText: 'Contraseña',
+                    hintText: 'Ingresa tu contraseña',
+                    textInputType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    suffixIcon: false,
+                  ),
+                  Padding (
+                    padding: EdgeInsets.symmetric(horizontal: 1.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: (){},
+                            child: Text('¿Olvidaste tu contraseña?',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),),
 
 
 
-                //Form
-                //Button
-                //TextButton
-              ],
+                        ),MyButtonForm(text: 'Login', onPressed: (){}
+                        ),
+
+
+
+
+                      ],
+                    ),
+                  ),
+                  //Form
+                  //Button
+                  //TextButton
+                ],
+              ),
             ),
-          ),
-        )
-      ),
-     );
+          )),
+    );
   }
 }
+
