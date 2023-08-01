af68c60ef02d8e6344a940e542f8c9fc7329a8bd
Juan Canon <jccaon44@misena.edu.co>
2023-08-01 16:35:00 -0500

    se creo la interfaz de login_view.dart y se configuro las dependencias xxxx ademas se actualizo el build.grade a la version 19

--- lib/app/presentation/widgets/my_buttom_form.dart
+++ lib/app/presentation/widgets/my_buttom_form.dart
@@ -23,7 +23,13 @@ class MyButtonForm extends StatelessWidget{
         decoration:  BoxDecoration(
           color: Theme.of(context).colorScheme.primary,
           borderRadius: BorderRadius.circular(6.0),
-
+          boxShadow:[
+          BoxShadow(
+            color:Theme.of(context).colorScheme.primary.withOpacity(0.2),
+            blurRadius: 7,
+            offset: const Offset(0,5 )
+          )
+        ]
         ),child: Text(text,
         style: TextStyle(
             color:Theme.of(context).colorScheme.onPrimary,
